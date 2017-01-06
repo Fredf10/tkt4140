9add0d2dd06729e036ca7bc58761448f42a30452
Fredrik <fredrik.efossan@gmail.com>
2017-01-06 12:54:33 +0100

    added a simple python file

---
+++ pythonExercises/simple.py
@@ -0,0 +1,12 @@
+""" In python different there are a lot of different packages that have different functionality. 
+    For instance the package numpy containt many functions etc. for handling linear algebra; vectors, matrices, solving systems of linear equations.."""
+
+import numpy as np
+
+a = np.array([1,2, 3])
+b = np.array([2, 4, 6])
+
+c = a + b
+print c
+print a*a
+print a**2 # a**2 -> a^2
